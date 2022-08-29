ee6ad1401c1daa03a85e701f9878bb58ea423ff0
tomasdarracq <110825915+tomasdarracq@users.noreply.github.com>
2022-08-29 10:38:23 -0300

    Update Libro.cs

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -2,27 +2,33 @@
 
 namespace SRP
 {
+
     public class Book
     {
 
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
+        
 
         public Book(String title, String author, String code)
         {
             this.Title = title;
             this.Author = author;
             this.Code = code;
+            
         }
 
-        public void ShelveBook(String sector, String shelve)
-        {
-            this.LibrarySector = sector;
-            this.LibraryShelve = shelve;
-        }
 
     }
+    
+      public class ShelveBook{
+          public string LibrarySector { get ; set; }
+          public string LibraryShelve { get ; set; }
+          
+          public (String sector, String shelve){
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+          }
+        }
 }
